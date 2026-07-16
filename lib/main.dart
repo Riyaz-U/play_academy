5759346544daaecf83e00035d248777500fcfb3b
Riyaz Uddin <riyazuddin@dailyobjects.com>
2026-07-16 22:00:32 +0530

    Added a gaurdian provider

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ import 'providers/payment_provider.dart';
 import 'providers/dashboard_provider.dart';
 import 'providers/drill_provider.dart';
 import 'providers/batch_provider.dart';
+import 'providers/guardian_provider.dart';
 import 'services/firestore_service.dart';
 
 void main() async {
@@ -42,6 +43,7 @@ class PlayAcademyApp extends StatelessWidget {
         ChangeNotifierProvider(create: (_) => DashboardProvider(FirestoreService())),
         ChangeNotifierProvider(create: (_) => DrillProvider(FirestoreService())),
         ChangeNotifierProvider(create: (_) => BatchProvider()),
+        ChangeNotifierProvider(create: (_) => GuardianProvider()),
       ],
       child: const _AppRouter(),
     );
---
+++ lib/providers/guardian_provider.dart
@@ -0,0 +1,52 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import '../models/player_model.dart';
+import '../services/firestore_service.dart';
+
+class GuardianProvider extends ChangeNotifier {
+  final FirestoreService _service = FirestoreService();
+
+  List<PlayerModel> _children = [];
+  PlayerModel? _selectedChild;
+  StreamSubscription? _subscription;
+
+  List<PlayerModel> get children => _children;
+  PlayerModel? get selectedChild => _selectedChild;
+
+  bool get hasMultipleChildren => _children.length > 1;
+  bool get hasChildren => _children.isNotEmpty;
+
+  void listen(String guardianUid) {
+    _subscription?.cancel();
+    _subscription =
+        _service.streamPlayersByGuardian(guardianUid).listen((list) {
+      _children = list;
+      // Keep selected child in sync; auto-select if only one child
+      if (_selectedChild != null) {
+        _selectedChild = list.where((p) => p.uid == _selectedChild!.uid).firstOrNull;
+      }
+      if (_selectedChild == null && list.length == 1) {
+        _selectedChild = list.first;
+      }
+      notifyListeners();
+    });
+  }
+
+  void selectChild(PlayerModel child) {
+    _selectedChild = child;
+    notifyListeners();
+  }
+
+  void clear() {
+    _subscription?.cancel();
+    _children = [];
+    _selectedChild = null;
+    notifyListeners();
+  }
+
+  @override
+  void dispose() {
+    _subscription?.cancel();
+    super.dispose();
+  }
+}
